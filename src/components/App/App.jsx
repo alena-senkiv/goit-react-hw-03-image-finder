88374371bd5f11991992b2abeaa247b84db403a2
alena-senkiv <alena.senkiv@gmail.com>
2022-01-23 23:28:38 +0200

    add components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,124 @@
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { fetchImg } from 'services/pixabay-api';
+import { Searchbar } from 'components/Searchbar';
+import { ImageGallery } from 'components/ImageGallery';
+import { LoadMoreBtn } from 'components/LoadMoreBtn';
+import { Loader } from 'components/Loader';
+import { ErrorSearch } from 'components/ErrorSearch';
+import { Modal } from 'components/Modal';
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+export default class App extends Component {
+  state = {
+    status: Status.IDLE,
+    searchQuery: '',
+    images: [],
+    page: 1,
+    error: null,
+    showModal: false,
+    modalImgProps: { url: '', alt: '' },
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const prevQuery = prevState.searchQuery;
+    const nextQuery = this.state.searchQuery;
+
+    if (prevQuery !== nextQuery) {
+      this.reset();
+      this.setState({ status: Status.PENDING });
+      await this.fetchImages(nextQuery);
+    }
+  }
+
+  fetchImages = query => {
+    const { page } = this.state;
+    fetchImg(query, page)
+      .then(({ hits }) => {
+        if (hits.length === 0) {
+          return Promise.reject(new Error('Oops! Nothing found'));
+        }
+
+        this.setState(prevState => ({
+          images: [...prevState.images, ...hits],
+          status: Status.RESOLVED,
+        }));
+      })
+      .catch(error => this.setState({ error, status: Status.REJECTED }));
+  };
+
+  reset = () => {
+    this.setState({ page: 1, images: [] });
+  };
+
+  handleLoadMoreBtnClick = () => {
+    const query = this.state.searchQuery;
+    this.incrementPage();
+    this.fetchImages(query);
+    this.scrollDown();
+  };
+
+  incrementPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  scrollDown = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }, 500);
+  };
+
+  handleSearchFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  handleImgClick = ({ largeImageURL: url, tags: alt }) => {
+    this.setState({ modalImgProps: { url, alt } });
+    this.toggleModal();
+  };
+
+  render() {
+    const {
+      status,
+      images,
+      error,
+      showModal,
+      modalImgProps: { url, alt },
+    } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSearchFormSubmit} />
+        {status === 'idle' && <></>}
+        {status === 'pending' && <Loader />}
+        {status === 'rejected' && <ErrorSearch message={error.message} />}
+        {status === 'resolved' && (
+          <>
+            {showModal && (
+              <Modal onClose={this.toggleModal} url={url} alt={alt} />
+            )}
+            <ImageGallery images={images} openModal={this.handleImgClick} />
+            <LoadMoreBtn handleLoadMore={this.handleLoadMoreBtnClick} />
+          </>
+        )}
+
+        <ToastContainer />
+      </>
+    );
+  }
+}
